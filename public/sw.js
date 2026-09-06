b5a7a5fd85b1f2a31e9b2cecf6858bbf2dbb4121
flyboi96 <corbin.xela@gmail.com>
2026-09-06 09:29:02 -0600

    release(v0.2.2-alpha): add publication deletion

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "recurrent-shell-v2";
+const CACHE = "recurrent-shell-v3";
 const ASSETS = ["/", "/manifest.webmanifest", "/icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
